50a94e84275e9ba9036564272574ee21f4271385
Andrew Douglass <andrew@wasteddaylight.com>
2014-05-23 14:38:38 -0700

    Added support for geojson.

--- publisher.js
+++ publisher.js
@@ -6,6 +6,7 @@ var app = express();
 
 app.use(logfmt.requestLogger());
 
+// Basic JSON
 app.get('/', function(req, res) {
 
       var data = { 
@@ -25,6 +26,58 @@ app.get('/', function(req, res) {
           event.longitude = parseFloat(item.lon);
 
           data.events.push(event);
+        });
+
+        // Build feed!
+        res.set('Content-Type', 'application/json');
+        res.send(data);
+
+        client.end();
+      });
+    
+});
+
+// GeoJSON
+app.get('/geojson', function(req, res) {
+
+      var data = { 
+      "type": "FeatureCollection",
+       "features": []
+      };
+
+      /* Single feature example
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point", 
+          "coordinates": [102.0, 0.5]
+        },
+        "properties": {
+          "prop0": "value0"
+        }
+      }
+      */
+      
+      var client = new pg.Client(connString);
+      client.connect();
+      
+      client.query("select * from events where creation > now() - interval '24' hour", function(err, result) {
+
+        result.rows.forEach(function(item) {
+          var feature = {
+            "type": "Feature",
+            "id": item.event_no,
+            "geometry": {
+              "type": "Point",
+              "coordinates": [parseFloat(item.lon), parseFloat(item.lat)]
+            },
+            "properties": {
+              "title": item.message,
+              "timestamp": item.creation
+            }
+          };
+
+          data.features.push(feature);
         });
 
         // Build feed!
